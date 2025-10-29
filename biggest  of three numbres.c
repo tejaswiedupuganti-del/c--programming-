59a69fe732882948844576dc284906a2abcece1d
tejaswiedupuganti-del <tejaswiedupuganti@gmail.com>
2025-10-29 19:25:47 +0530

    Add files via upload

---
+++ biggest  of three numbres.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter a,b,c values");
+	scanf("%d%d%d",&a,&b,&c);
+	if(a>b)
+	{
+		if (a>c)
+		printf ("c is big");
+	
+	}
+	else
+	{
+		if (b>c)
+		printf("b is big");
+		else
+		printf("c is big");
+	}
+	return 0;
+}
